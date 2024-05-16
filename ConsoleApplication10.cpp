40c304cf331c9fcfd881c2e228cb7eda47bc43c5
Hxrdbvss <157516489+Hxrdbvss@users.noreply.github.com>
2024-05-16 14:35:51 +0300

    Add files via upload

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+void threadF() {
+    for (int i = 0; i < 10; ++i) {
+        cout << "Semi stream: " << i << endl;
+    }
+}
+
+int main() {
+    thread t(threadF);
+    for (int i = 0; i < 10; ++i) {
+        cout << "Main stream: " << i << endl;
+    }
+    t.join(); 
+    return 0;
+}
